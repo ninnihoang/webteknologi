4ec40e6c6d87e017fa756d5f18eb3d3a89898a27
casparmkerr <casparmkerr@gmail.com>
2017-10-17 11:33:12 +0200

    Adds skill funtionality

--- Designhjelpen/scripts/quoteSkills.js
+++ Designhjelpen/scripts/quoteSkills.js
@@ -1,1 +1,7 @@
-document.getElementById("skill").innerHTML = "test";
+let verb = ['planlegge', 'iterere', 'visualisere', 'analysere', 'bearbeide', 'gjennomføre', 'klassifisere', 'designe', 'undersøke', 'hjelpe', 'utvikle', 'fremsnakke', 'gjennomgå', 'researche']
+let adjektiv = ['ikke-linjære', 'fremtidsrettede', 'ikke-visualiserbare', 'postmoderne', 'komplekse', 'alternative', 'grensesprengende', 'disruptive', 'nyutviklede', 'selvreplikerende', 'nyskapende', 'iterative', 'levedyktige']
+let substantiv = ['brukeropplevelser', 'workshops', 'produkter', 'gjennombrudd', 'problemstillinger', 'utstillinger', 'systemer', 'UIs', 'start-ups', 'AI', 'konsepter', 'design thinkers', 'boot camps', 'iterasjoner']
+
+
+
+document.getElementById("skill").innerHTML = ('Vi kan ' + verb[Math.floor(Math.random() * verb.length)] + ' ' + adjektiv[Math.floor(Math.random() * adjektiv.length)] + ' ' + substantiv[Math.floor(Math.random() * substantiv.length)]);
