fb47d439e63eee11d5d7925068962985a350fc2f
Ninni Kim Nhi Hoang <ninnihoang@gmail.com>
2017-10-23 13:10:39 +0200

    Ferdig med mine grunnleggende deler

--- Designhjelpen/scripts/validateForms.js
+++ Designhjelpen/scripts/validateForms.js
@@ -3,27 +3,29 @@ var name,
     email,
     website;
 
-name = document.getElementById("contact");
+contact = document.getElementById("contact");
 telephone = document.getElementById("telephone");
 email = document.getElementById("email");
 website = document.getElementById("website");
 
 function formValidation() {
-    if (telephone.checkValidity() == false) {
+    if (contact.checkValidity() === false) {
+        alert("Vennligst fyll inn et navn");
+        contact.focus();
+    } else if (telephone.checkValidity() === false) {
         alert("Vennligst fyll inn et gyldig telefonnummer");
         telephone.focus();
-    } else if (email.checkValidity() == false) {
+    } else if (email.checkValidity() === false) {
         alert("Vennligst fyll inn en gyldig e-postadresse");
         email.focus();
-    } else if (website.checkValidity() == false) {
+    } else if (website.checkValidity() === false) {
         alert("Vennligst fyll inn en gyldig nettside");
         website.focus();
+    } else {
+        alert("Takk for din henvendelse! \n Vi vil kontakte deg så fort som mulig!")
     }
+    console.log(contact);
+    console.log(document.getElementById("contact"));
 }
 
-
-document.getElementById('submit').addEventListener('click', function () {
-    if (formValidation()) {
-        window.alert('Takk for din henvendelse! \n Vi vil kontakte deg så fort som mulig via email! \n');
-    }
-});
+document.getElementById('submit').addEventListener('click', formValidation)
