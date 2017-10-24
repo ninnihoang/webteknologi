3a8807c94f9aac141f09521a996b110feac59646
Mia Edvarden <miaedvardsen@yahoo.com>
2017-10-24 18:34:19 +0200

    bytte bilde kilder

--- Designhjelpen/scripts/ChangePhotos.js
+++ Designhjelpen/scripts/ChangePhotos.js
@@ -6,14 +6,14 @@ var imageClassname=["image1","image2","image3"];
 function changePhotos(){
   if (hour > 16){ //after 4 at noon
     photosNow.className += " " + imageClassname[0]; // add a image class in img
-    photosNow.src = "img/outsideID1.jpg";
+    photosNow.src = "img/ipd1.jpg";
   } else if (hour > 8) { // after 8 in the morning
     photosNow.className += " " + imageClassname[1];
-    photosNow.src = "img/outsideID2.jpg";
+    photosNow.src = "img/ipd2.jpg";
 
   }else{ // other times
     photosNow.className += " " + imageClassname[2];
-    photosNow.src ="img/outsideID13.jpg";
+    photosNow.src ="img/ipd3.jpg";
   }
 }
 changePhotos();
