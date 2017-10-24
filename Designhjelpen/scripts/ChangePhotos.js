a2081d53f61149e1996800c670c826545093f765
Mia Edvarden <miaedvardsen@yahoo.com>
2017-10-24 18:25:40 +0200

    continued work on photos

--- Designhjelpen/scripts/ChangePhotos.js
+++ Designhjelpen/scripts/ChangePhotos.js
@@ -1,22 +1,19 @@
-var date = new date();
+var date = new Date();
 var hour = date.getHours();
 var photosNow = document.getElementById("changePhotos");
 var imageClassname=["image1","image2","image3"];
 
 function changePhotos(){
-  if (hour > 8){ //after 8 in the morning
+  if (hour > 16){ //after 4 at noon
     photosNow.className += " " + imageClassname[0]; // add a image class in img
     photosNow.src = "img/outsideID1.jpg";
-  } else if (hour > 16) { // after 4 at noon
+  } else if (hour > 8) { // after 8 in the morning
     photosNow.className += " " + imageClassname[1];
     photosNow.src = "img/outsideID2.jpg";
 
-  }else if (hour > 0){ // after midtnight
-    photosNow.className += " " + imageClassname[2];
-    photosNow.src = "img/outsideID3.jpg";
   }else{ // other times
-    photosNow.className += " " + imageClassname[0];
-    photosNow.src ="img/outsideID1.jpg";
+    photosNow.className += " " + imageClassname[2];
+    photosNow.src ="img/outsideID13.jpg";
   }
-
 }
+changePhotos();
