ebb146eb96166929bd8be1530e612a145992fdf6
Ninni Kim Nhi Hoang <ninnihoang@gmail.com>
2017-11-03 08:08:12 +0100

    fikset validateForms slik at den også fungerer for application

--- Designhjelpen/scripts/validateForms.js
+++ Designhjelpen/scripts/validateForms.js
@@ -2,19 +2,19 @@ var contact = document.getElementById("contact");
 var telephone = document.getElementById("telephone");
 var email = document.getElementById("email");
 var website = document.getElementById("website");
-var submit = document.getElementsByClassName("buttonPink");
+var submit = document.getElementsByClassName("formSubmit");
 
 function formValidation() {
-    if (contact.checkValidity() === false) {
+    if (contact !== null && contact.checkValidity() === false) {
         alert("Vennligst fyll inn et gyldig navn");
         contact.focus();
-    } else if (telephone.checkValidity() === false) {
+    } else if (telephone !== null && telephone.checkValidity() === false) {
         alert("Vennligst fyll inn et gyldig telefonnummer");
         telephone.focus();
-    } else if (email.checkValidity() === false) {
+    } else if (email !== null && email.checkValidity() === false) {
         alert("Vennligst fyll inn en gyldig e-postadresse");
         email.focus();
-    } else if (website.checkValidity() === false) {
+    } else if (website !== null && website.checkValidity() === false) {
         alert("Vennligst fyll inn en gyldig nettside");
         website.focus();
     } else {
@@ -23,6 +23,8 @@ function formValidation() {
 }
 
 console.log(submit);
+console.log(contact !== null);
+console.log(contact.checkValidity() === false);
 
 for (var i = 0; i < submit.length; i++) {
     submit[i].addEventListener('click', formValidation);
