0a6042b79e367e7ce5a280b8b36095b1d2aa966b
Mia Edvarden <miaedvardsen@yahoo.com>
2017-11-07 18:07:20 +0100

    fikset home

---
+++ Designhjelpen/scripts/slider.js
@@ -0,0 +1,20 @@
+var sliderIndex = 1;
+showSlider (sliderIndex); // display first image
+
+function plusSlider(n){
+  showSlider (sliderIndex += n);
+}
+
+
+function showSlider(n){
+  var i = 0;
+  var x = document.getElementsByClassName("sliderImg");
+  if(n > x.length){sliderIndex = 1} //reset the slideIndex
+  if(n < 1){sliderIndex = x.length};
+  console.log(n); // show the last image
+  for(i = 0; i < x.length; i++){
+    x[i].style.display = "none" ; // hide images
+  }
+  x[sliderIndex-1].style.display = "block"; //show image with given slideIndex
+  
+}
